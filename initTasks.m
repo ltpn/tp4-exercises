0a5a8f12acc460c345e65b94eba27141611ef71f
Matteo Seclì <secli.matteo@gmail.com>
2024-09-27 11:22:54 +0200

    🧹 Cleanup

--- initTasks.m
+++ initTasks.m
@@ -3,7 +3,7 @@ if ( exist('OCTAVE_VERSION', 'builtin') ~= 0 ) && ( exist("datetime") == 0 )
     try
         pkg load tablicious
     catch
-        pkg install "tablicious-0.4.3.tar.gz"
+        pkg install -forge tablicious
         pkg load tablicious
     end
 end
